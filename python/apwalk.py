185c141cb6e192a1ec538402edb4c2bbeecdeed9
drsputnik <dr.sputnik@gmail.com>
2019-01-30 11:14:46 +0000

    Added fucntion

--- python/apwalk.py
+++ python/apwalk.py
@@ -1,14 +1,17 @@
 #import curses
-stdscr = curses.initscr()                       # Initialise stdscr as a curses window
-curses.noecho()                                 # Settings to make it terminal friendly
-curses.cbreak()
-stdscr.keypad(1)
+def init_screen():
+	stdscr = curses.initscr()			# Initialise stdscr as a curses window
+	curses.noecho()					# Settings to make it terminal friendly
+	curses.cbreak()
+	stdscr.keypad(1)
 
-row = 0
-col = 0
+	#row = 0
+	#col = 0
 
-row, col = stdscr.getmaxyx()                    # Get the size of the terminal window
+	r, c = stdscr.getmaxyx()			# Get the size of the terminal window
+	return r, c
 
+row, col = init_screen();
 stdscr.addstr(row-2, 0, "This screen has " + str (row) + " rows and " + str (col) + " columns.")
 stdscr.addstr("Try resizing your window(if possible) and then run this program again")
 stdscr.refresh()
